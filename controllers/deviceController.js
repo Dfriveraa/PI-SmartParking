8d01dfa3d1853c1c3783cfed99f5b7b1bb1aec0c
Daniel rivera <Danielf.r97@gmail.com>
2020-02-06 06:36:26 -0500

    Model device

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -18,14 +18,19 @@ const createDevice=(req,res)=>{
   console.log("connect");
    var device=new deviceModel();
 
-   device._id=1;
+   device._id=450;
    device.recorder_id="Juan pablo";
    device.canvas_location.y=2;
    device.canvas_location.x=1;
+   device.real_location.sector="Bloque 16";
+   device.real_location.identifier=1;
+   device.app_eui="45648"
+   device.date=new Date();
+   device.save();
    // device._id=req.body._id*1;
    // device.title=req.body.title;
    // device.author=req.body.author;
-    device.save();
+    //device.save();
     res.json({
         status:"Entro al create",
         message:"try again more later",
--- models/devicesModel.js
+++ models/devicesModel.js
@@ -24,6 +24,27 @@ const device = new Schema(
         required: [true,'Coordenada y'] ,
         trim: true
       }
+    },    
+    real_location:{
+      sector:{
+      type:String,
+      required: [true,'Debe indicar a que parqueadero pertenece el dispositivo'] ,
+      trim: true
+    },
+      identifier:{
+        type:Number,
+        required: [true,'Debe indicar con '] ,
+        trim: true
+      }
+    },
+    app_eui:{
+      type:String,
+      required: [true,'Un dispostivo debe tener un appEui'],
+      trim:true
+    },
+    date:{
+      type:Date,
+      required:[true,'Fecha en la que se registró del dispositivo']
     }
   },{
     versionKey: false // You should be aware of the outcome after set to false
